da8ea7495d5a9e3d66dd6a94a72554148f59492c
ommarra <otavio755@gmail.com>
2018-05-01 20:29:52 -0400

    added error and show pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,16 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect -> it render the inputed information into a "ruby readable" form
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+   if  @article.save
+     flash[:notice] = "Article was created"
+    redirect_to article_path(@article)
+   else 
+     render 'new'
+   end
+  end
+  
+  def show 
+    @article = Article.find(params[:id])
   end
   
   private
